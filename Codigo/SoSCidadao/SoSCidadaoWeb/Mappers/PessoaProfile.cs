bb05f59d780f0fc3294e9fed399d8a83b241cf80
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-14 13:30:12 -0300

    #68 - criacao do mapper de pessoa

---
+++ Codigo/SoSCidadao/SoSCidadaoWeb/Mappers/PessoaProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core;
+using SoSCidadaoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoSCidadaoWeb.Mappers
+{
+    public class PessoaProfile : Profile
+    {
+        public PessoaProfile() 
+        {
+            CreateMap<PessoaModel, Pessoa>().ReverseMap();
+        }
+    }
+}
