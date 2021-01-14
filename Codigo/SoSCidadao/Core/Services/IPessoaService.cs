0674e3137c9a41eb579939ac4e834db6756803e3
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-14 12:41:12 -0300

    #68 - cricao da interface de pessoa

---
+++ Codigo/SoSCidadao/Core/Services/IPessoaService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public interface IPessoaService
+    {
+        int Inserir(Pessoa pessoa);
+
+        Pessoa Obter(Pessoa pessoa);
+
+        int Atualizar(Pessoa pessoa);
+
+        int Remover(Pessoa pessoa);
+
+        bool Validar(Pessoa pessoa);
+
+    }
+}
