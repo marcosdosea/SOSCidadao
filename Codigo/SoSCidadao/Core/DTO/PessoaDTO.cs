361f2c4e8ded645d09eb902c6dec7f92360ec7da
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-14 12:40:41 -0300

    #68 - cricao da interface de pessoa

---
+++ Codigo/SoSCidadao/Core/DTO/PessoaDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DTO
+{
+    public class PessoaDTO
+    {
+
+        public int IdPessoa { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+
+        public string StatusPessoa { get; set; }
+
+        public int IdOrganizacao { get; set; }
+
+    }
+}
