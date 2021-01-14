a343712ebeb2c421a1dc0eb67b16995477aabd45
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-14 11:52:34 -0300

    resolve #102 - correção de erro na startup.cs da string de conexao

--- Codigo/SoSCidadao/SoSCidadaoWeb/Startup.cs
+++ Codigo/SoSCidadao/SoSCidadaoWeb/Startup.cs
@@ -24,9 +24,6 @@ namespace SoSCidadaoWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SosCidadaoContext>(options =>
-                options.UseMySql(Configuration.GetConnectionString("SosCidadaoConnection")));
-
             services.AddControllersWithViews();
         }
 
