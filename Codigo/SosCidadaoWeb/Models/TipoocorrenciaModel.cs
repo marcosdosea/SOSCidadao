604401ba5d10f95cd81e3d7ad628a5d558cb0890
Roberto S Jesus <eu.robertosantana@gmail.com>
2021-01-18 17:46:20 -0300

    #126 - cricao da ViewModel e Mappers de tipoOcorrencia

---
+++ Codigo/SosCidadaoWeb/Models/TipoocorrenciaModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SosCidadaoWeb.Models
+{
+    public class TipoocorrenciaModel
+    {
+        [Display(Name = "Número cadastral")]
+        [Key]
+        [Required(ErrorMessage = "Não foi possivel identificar o número cadastral do tipo")]
+        public int IdTipoOcorrencia { get; set; }
+
+        [Display(Name = "Nome do Tipo da Ocorrência")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Campo Nome deve possuir no mínimo 5 caracteres.")]
+        [Required(ErrorMessage = "Campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Número IdOrganizacao")]
+        [Key]
+        [Required(ErrorMessage = "Não foi possivel identificar o IdOrganizacao")]
+        public int IdOrganizacao { get; set; }
+    }
+}
