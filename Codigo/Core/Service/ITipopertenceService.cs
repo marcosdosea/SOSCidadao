47d30b4c48542308026fe23ffda0abcc39068ead
WedsonTeixeira <wedson.teixeira1998@outlook.com>
2021-01-18 10:18:12 -0300

    Resolve #121 - Adicionando a classe service de Core

---
+++ Codigo/Core/Service/ITipopertenceService.cs
@@ -0,0 +1,16 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Service
+{
+    public interface ITipopertenceService
+    {
+		int Inserir(Tipopertence tipopertence);
+		Tipopertence Obter(int idTipopertence);
+		IEnumerable<Tipopertence> ObterTodos();
+		void Atualizar(Tipopertence tipopertence);
+		void Remover(int idTipopertence);
+    }
+}
