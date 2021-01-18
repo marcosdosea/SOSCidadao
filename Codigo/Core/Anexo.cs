aa35e77e8aa48ac1568d2bb0e787c038d4259e00
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-18 00:39:13 -0300

    #117 - correção dos arquivos

---
+++ Codigo/Core/Anexo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class Anexo
+    {
+        public int IdAnexoPertence { get; set; }
+        public string Nome { get; set; }
+        public string UrlArquivo { get; set; }
+        public DateTime? DataCadastro { get; set; }
+        public int IdOcorrencia { get; set; }
+        public int IdPessoa { get; set; }
+
+        public virtual Ocorrencia IdOcorrenciaNavigation { get; set; }
+        public virtual Pessoa IdPessoaNavigation { get; set; }
+    }
+}
---
+++ Codigo/Core/Comentario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class Comentario
+    {
+        public int IdComentario { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Descricao { get; set; }
+        public int IdOcorrencia { get; set; }
+        public int IidPessoa { get; set; }
+
+        public virtual Ocorrencia IdOcorrenciaNavigation { get; set; }
+        public virtual Pessoa IidPessoaNavigation { get; set; }
+    }
+}
