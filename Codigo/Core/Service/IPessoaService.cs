443f9c5be637312ec5a373f31d7e2061b97dfa67
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-18 13:42:39 -0300

    resolve #68 - correcoes ao editar e cadastrar usuario, bem como links renderizados com razor para os menus

---
+++ Codigo/Core/Service/IPessoaService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Service
+{
+    public interface IPessoaService
+    {
+
+        int Inserir(Pessoa pessoa);
+
+        Pessoa Obter(int id);
+
+        IEnumerable<Pessoa> ObterTodos();
+
+        int Atualizar(Pessoa pessoa);
+
+        int Remover(int id);
+
+        bool Validar(Pessoa pessoa);
+    }
+}
---
+++ Codigo/SosCidadaoWeb/wwwroot/js/criar_conta.js
@@ -0,0 +1,38 @@
+﻿
+$(document).ready(function () {
+    $('#bt_create').click(function () {
+        if ($(this).html().trim() == 'Continuar') {
+            $('#box-main').removeClass('col-12');
+            $('#box-main').addClass(['col-6', 'd-flex', 'justify-content-center', 'flex-column']);
+
+            $('#data-personal').toggle();
+            $('#data-user').removeClass('d-none');
+
+            $('#step').html('Etapa 2');
+            $(this).html('Finalizar')
+        } else if ($(this).html().trim() == 'Finalizar') {
+            $('#form-create').submit();
+        }
+    });
+    $('#btn_back').click(function (e) {
+        if ($('#step').html().trim() == 'Etapa 2') {
+            e.preventDefault()
+            $('#box-main').addClass('col-12');
+            $('#box-main').removeClass(['col-6', 'd-flex', 'justify-content-center', 'flex-column']);
+
+            $('#data-personal').toggle();
+            $('#data-user').addClass('d-none');
+
+            $('#step').html('Etapa 1');
+            $('#bt_create').html('Continuar');               
+                
+            $(this).removeAttr('asp-controller');
+            $(this).removeAttr('asp-action');
+        }
+
+    });
+
+
+
+
+});
