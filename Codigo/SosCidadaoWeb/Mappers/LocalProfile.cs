df0a161924023936560fe31c95eacc9fc3ed5b76
Roberto S Jesus <eu.robertosantana@gmail.com>
2021-01-21 22:07:02 -0300

    #136 - cricao da class Mapper de Local

---
+++ Codigo/SosCidadaoWeb/Mappers/LocalProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using SosCidadaoWeb.Models;
+
+namespace SosCidadaoWeb.Mappers
+{
+    public class LocalProfile: Profile
+    {
+        public LocalProfile()
+        {
+            CreateMap<LocalModel, Local>().ReverseMap();
+        }
+    }
+}
