f67eac1b7aa0e27cfd1c62b9452300b2a6f7511a
Roberto S Jesus <eu.robertosantana@gmail.com>
2021-01-21 21:24:32 -0300

    #136 - cricao da interface, Service e DTO de Local

---
+++ Codigo/Core/DTO/LocalDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DTO
+{
+    public class LocalDTO
+    {
+        public int IdLocal { get; set; }
+        public string Nome { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public string Cep { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+        public int? NumeroEndereco { get; set; }
+        public int IdOrganizacao { get; set; }
+    }
+}
