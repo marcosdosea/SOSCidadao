d0e17c815897c7cc9c633a772fc49ad0cf6a1c88
Dryele <dryele.an@gmail.com>
2021-01-29 14:29:06 -0300

    Implementaçao da OrganizacaoModel.cs

---
+++ Codigo/SosCidadaoWeb/Models/OrganizacaoModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SosCidadaoWeb.Models
+{
+    public class OrganizacaoModel
+    {
+        
+        [Display(Name = "Código")]
+        [Key]
+        [Required(ErrorMessage = "Campo Código é obrigatório.")]
+        public int IdOrganizacao { get; set; }
+
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Campo Nome deve possuir no mínimo 5 caracteres.")]
+        [Required(ErrorMessage = "Campo Cnpj é obrigatório.")]
+        public string Cnpj { get; set; }
+
+        [Display(Name = "Razao Social da Organização")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Campo Nome deve possuir no mínimo 5 caracteres.")]
+        [Required(ErrorMessage = "Campo Razão Social é obrigatório.")]
+        public string NomeRazao { get; set; }
+
+        [Display(Name = "Nome Fantasia da Organização")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Campo Nome deve possuir no mínimo 5 caracteres.")]
+        [Required(ErrorMessage = "Campo Nome Fantasia é obrigatório.")]
+        public string NomeFantasia { get; set; }
+
+        [Display(Name = "CEP")]
+        [StringLength(8, ErrorMessage = "Campo CEP está incompleto", MinimumLength = 8)]
+        [Required(ErrorMessage = "Campo CEP é obrigatório.")]
+        public string Cep { get; set; }
+
+        [Display(Name = "Rua/Avenida")]
+        [StringLength(45, ErrorMessage = "Campo Rua/Avenida não permite mais que 45 caracteres")]
+        [Required(ErrorMessage = "Campo Rua/Avenida é obrigatório.")]
+        public string Rua { get; set; }
+
+        [Display(Name = "Bairro")]
+        [StringLength(45, ErrorMessage = "Campo Bairro não permite mais que 45 caracteres")]
+        [Required(ErrorMessage = "Campo Bairro é obrigatório.")]
+        public string Bairro { get; set; }
+
+        [Display(Name = "Cidade")]
+        [StringLength(45, ErrorMessage = "Campo Cidade não permite mais que 45 caracteres")]
+        [Required(ErrorMessage = "Campo Cidade é obrigatório.")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "UF")]
+        [StringLength(3, ErrorMessage = "Campo UF não permite mais que 2 caracteres")]
+        [Required(ErrorMessage = "Campo UF é obrigatório.")]
+        public string Uf { get; set; }
+
+        [Display(Name = "Nº")]
+        [Range(0, int.MaxValue, ErrorMessage = "Campo Número do Endereço é inválido")]
+        public int NumeroEndereco { get; set; }
+
+        public DateTime DataRegistro { get; set; }
+
+        public int? IdPessoa { get; set; }
+
+
+    }
+}
+
+
+
+public DateTime DataRegistro { get; set; }
+public int IdPessoa { get; set; }
