bbc840ece1bf56f05e526fb29b42ffed31a70879
Dryele <dryele.an@gmail.com>
2021-01-30 09:50:40 -0300

    Implementação de todas views da Organização

---
+++ Codigo/SosCidadaoWeb/Controllers/OrganizacaoController.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Mvc;
+using SosCidadaoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SosCidadaoWeb.Controllers
+{
+    public class OrganizacaoController : Controller
+    {
+        
+        IOrganizacaoService _organizacaoService;
+        IMapper _mapper;
+
+        public OrganizacaoController(IOrganizacaoService organizacaoService, IMapper mapper)
+        {
+            _organizacaoService = organizacaoService;
+            _mapper = mapper;
+        }
+
+        // GET: OrganizacaoController
+        public ActionResult Index()
+        {
+            var listaOrganizacoes = _organizacaoService.ObterTodos();
+            var listaOrganizacoesModel = _mapper.Map<List<OrganizacaoModel>>(listaOrganizacoes);
+            return View(listaOrganizacoesModel);
+        }
+
+        // GET: OrganizacaoController/Details/5
+        public ActionResult Details(int id)
+        {
+            Organizacao organizacao = _organizacaoService.Obter(id);
+            OrganizacaoModel organizacaoModel = _mapper.Map<OrganizacaoModel>(organizacao);
+            return View(organizacaoModel);
+        }
+
+        // GET: OrganizacaoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrganizacaoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(OrganizacaoModel organizacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var organizacao = _mapper.Map<Organizacao>(organizacaoModel);
+                _organizacaoService.Inserir(organizacao);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: OrganizacaoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Organizacao organizacao = _organizacaoService.Obter(id);
+            OrganizacaoModel organizacaoModel = _mapper.Map<OrganizacaoModel>(organizacao);
+            return View(organizacaoModel);
+        }
+
+        // POST: OrganizacaoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (int id, OrganizacaoModel organizacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var organizacao = _mapper.Map<Organizacao>(organizacaoModel);
+                _organizacaoService.Atualizar(organizacao);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: OrganizacaoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Organizacao organizacao = _organizacaoService.Obter(id);
+            OrganizacaoModel organizacaoModel = _mapper.Map < OrganizacaoModel>(organizacao);
+            return View(organizacaoModel);
+        }
+
+        // POST: OrganizacaoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete (int id, OrganizacaoModel organizacaoModel)
+        {
+            _organizacaoService.Remover(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
