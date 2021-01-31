6d5bc8ba8f1fd93a616792c1f33737ff6e2f89a9
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-01-31 19:25:53 -0300

    #149 - adicionado novo atributo ao DTO e criado um metodo para associar o tipo do pertence

--- Codigo/Core/Service/IPertenceService.cs
+++ Codigo/Core/Service/IPertenceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +11,8 @@ namespace Core.Service
         int Inserir(Pertence pertence);
 
         Pertence Obter(int idPertence);
+
+        PertenceDTO ObterDto(int idPertence);
 
         IEnumerable<Pertence> ObterTodos();
 
