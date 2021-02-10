1019076c773d2b3f691d03bbc6bf7406e2a18254
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-02-10 00:24:40 -0300

    #163 - validações de cadastro e finalização da tela de login

--- Codigo/SosCidadaoWeb/Areas/Identity/IdentityHostingStartup.cs
+++ Codigo/SosCidadaoWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -19,9 +20,49 @@ namespace SosCidadaoWeb.Areas.Identity
                     options.UseMySQL(
                         context.Configuration.GetConnectionString("SosCidadaoConnection")));
 
-                services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = true)
+     
+                services.AddDefaultIdentity<Usuario>(options =>
+                {
+                    // Configurações para SignIn
+                    options.SignIn.RequireConfirmedEmail = false;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                    // Configurações da Senha
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 6;
+                    //options.Password.RequiredUniqueChars = 1;
+
+                    // Configurações do Usuário 
+                    options.User.AllowedUserNameCharacters =
+                            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                    options.User.RequireUniqueEmail = true;
+
+                    // Configurações de Bloqueio do usuário
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+                    options.Lockout.AllowedForNewUsers = true;
+
+                }).AddRoles<IdentityRole>()
                     .AddEntityFrameworkStores<IdentityContext>();
+
+
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                    options.Cookie.Name = "SosCidadaoCookieUsuario";
+                    options.Cookie.HttpOnly = true;
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                    options.LoginPath = "/Identity/Account/Login";
+                    // ReturnUrlParameter requires 
+                    //using Microsoft.AspNetCore.Authentication.Cookies;
+                    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                    options.SlidingExpiration = true;
+                });
             });
+
         }
     }
 }
