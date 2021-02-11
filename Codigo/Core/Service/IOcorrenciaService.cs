e87964fb4811009af7237500d233b7cb872fd927
Dryele <dryele.an@gmail.com>
2021-02-11 08:51:12 -0300

    Resolve #168, Imprelentação da Interface OcorrenciaService.cs e da Classe Service/OcorrenciaService.cs.

---
+++ Codigo/Core/Service/IOcorrenciaService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Service
+{
+    public interface IOcorrenciaService
+    {
+        int Inserir(Ocorrencia ocorrencia);
+
+        Ocorrencia Obter(int id);
+
+        IEnumerable<Ocorrencia> ObterTodos();
+
+        int Atualizar(Ocorrencia ocorrencia);
+
+        int Remover(int id);
+
+    }
+}
+
---
+++ Codigo/Service/OcorrenciaService.cs
@@ -0,0 +1,62 @@
+﻿using Core;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service
+{
+    class OcorrenciaService : IOcorrenciaService
+    {
+        private readonly SosCidadaoContext _context;
+
+        public OcorrenciaService(SosCidadaoContext context)
+        {
+            _context = context;
+        }
+
+        public int Inserir(Ocorrencia ocorrencia)
+        {
+            _context.Add(ocorrencia);
+            _context.SaveChanges();
+            return ocorrencia.IdOcorrencia;
+        }
+
+        public int Atualizar(Ocorrencia ocorrencia)
+        {
+            _context.Update(ocorrencia);
+            return _context.SaveChanges();
+
+        }
+
+        public Ocorrencia Obter(int id)
+        {
+            var _ocorrencia = _context.Ocorrencia.Find(id);
+            return _ocorrencia;
+        }
+
+        public int Remover(int id)
+        {
+            var _ocorrencia = _context.Ocorrencia.Find(id);
+            _context.Ocorrencia.Remove(_ocorrencia);
+            return _context.SaveChanges();
+        }
+
+
+        private IQueryable<Ocorrencia> GetQuery()
+        {
+            IQueryable<Ocorrencia> tb_ocorrencia = _context.Ocorrencia;
+            var query = from ocorrencia in tb_ocorrencia
+                        select ocorrencia;
+            return query;
+
+        }
+
+        public IEnumerable<Ocorrencia> ObterTodos()
+        {
+            return GetQuery();
+        }
+
+    }
+}
