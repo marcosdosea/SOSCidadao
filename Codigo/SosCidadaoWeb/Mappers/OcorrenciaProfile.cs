7a9ae58fbd09bf83d6de7e9951aa5a96448757ba
Dryele <dryele.an@gmail.com>
2021-02-12 11:11:21 -0300

    Classe OcorrenciaProfile.cs implementada

---
+++ Codigo/SosCidadaoWeb/Mappers/OcorrenciaProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core;
+using SosCidadaoWeb.Models;
+
+namespace SosCidadaoWeb.Mappers
+{
+    public class OcorrenciaProfile: Profile
+    {
+        public OcorrenciaProfile()
+        {
+            CreateMap<OcorrenciaModel, Ocorrencia>().ReverseMap();
+        }
+        
+    }
+}
