888a6ebce6f9d888dba57271f188494b236ab16b
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-02-11 23:46:33 -0300

    #163 - alinhamento padrao do main

--- Codigo/SosCidadaoWeb/Areas/Identity/Data/Usuario.cs
+++ Codigo/SosCidadaoWeb/Areas/Identity/Data/Usuario.cs
@@ -9,5 +9,6 @@ namespace SosCidadaoWeb.Areas.Identity.Data
     // Add profile data for application users by adding properties to the Usuario class
     public class Usuario : IdentityUser
     {
+
     }
 }
