1f42bffe3683ba83fac3a91e73defc853e402a67
Dryele <dryele.an@gmail.com>
2021-02-15 09:33:41 -0300

    Implementaçãos de ajustes solicitados da review

--- Codigo/SosCidadaoWeb/Models/OcorrenciaModel.cs
+++ Codigo/SosCidadaoWeb/Models/OcorrenciaModel.cs
@@ -8,31 +8,27 @@ namespace SosCidadaoWeb.Models
 {
     public class OcorrenciaModel
     {
-        [Display(Name = "Código da Ocorrência")]
+        [Display(Name = "Nº Cadastral")]
         [Key]
-        [Required(ErrorMessage = "Campo código é obrigatório.")]
         public int? IdOcorrencia { get; set; }
 
+
+        [Display(Name = "Data")] 
         public DateTime? DataOcorrencia { get; set; }
 
-        [Display(Name = "Descrição da Ocorrência")]
+        [Display(Name = "Descrição")]
         [StringLength(400, MinimumLength = 5, ErrorMessage = "Campo Descrição deve possuir no mínimo 5 caracteres.")]
         [Required(ErrorMessage = "Campo Descrição é obrigatório.")]
         public string Descricao { get; set; }
 
-        [Display(Name = "Status da Ocorrência")]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Campo Status deve possuir no mínimo 5 caracteres.")]
-        [Required(ErrorMessage = "Campo Status é obrigatório.")]
+       
         public string StatusOcorrencia { get; set; }
 
         [Display(Name = "Telefone")]
-        [StringLength(15, MinimumLength = 5, ErrorMessage = "Campo Telefone deve possuir no mínimo 5 caracteres.")]
-        [Required(ErrorMessage = "Campo telefone é obrigatório.")]
+        [StringLength(15, MinimumLength = 9, ErrorMessage = "Campo Telefone deve possuir no mínimo 9 caracteres.")]
         public string Telefone { get; set; }
 
         [Display(Name = "Emergência")]
-        [StringLength(10, ErrorMessage = "Campo Emergencia está incompleto", MinimumLength = 3)]
-        [Required(ErrorMessage = "Campo Emergencia é obrigatório.")]
         public string Emergencia { get; set; }
     }
 }
