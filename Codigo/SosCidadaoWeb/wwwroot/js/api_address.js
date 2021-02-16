a010785a9f8c53997cef4a59e6d15faac01703eb
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2021-02-16 11:23:49 -0300

    resolve #175 - exibir mensagem de erro abaixo do campo de cep

--- Codigo/SosCidadaoWeb/wwwroot/js/api_address.js
+++ Codigo/SosCidadaoWeb/wwwroot/js/api_address.js
@@ -1,12 +1,12 @@
 ﻿$(function () {
 
-    $('#busca_cep').click(function () {
-        $("#msg-cep").text('');
+    $('#search_cep').click(function () {
+        $('#msg-cep').text('');
         var cep = $('#cep').val();
         var url = `viacep.com.br/ws/${cep}/json`;
 
         if (cep == '') {
-            alert('Informe o Cep antes de continuar');
+            $('#msg-cep').html('* Informe o Cep antes de continuar.');
             $('#cep').focus();
             return false;
         }
@@ -15,22 +15,29 @@
             url: 'https://viacep.com.br/ws/' + cep + '/json',
             dataType: 'json',
             crossDomain: true,
-            contentType: "application/json",
+            contentType: 'application/json',
+            beforeSend: function () {
+                $('#search_cep').addClass("disabled");    
+            },
             statusCode: {
                 200: function (data) {
+                    if (data.erro) {
+                        $("#msg-cep").html("* CEP não encontrado."); cleanAddress()
+                    }
                     $('#uf').val(data.uf)
                     $('#cidade').val(data.localidade)
                     $('#bairro').val(data.bairro)
                     $('#rua').val(data.logradouro)
                 } // Ok
-                , 400: function (msg) { $("#msg-cep").html("CEP não encontrado!!"); cleanAddress() } // Bad Request
-                , 404: function (msg) { $("#msg-cep").html("CEP não encontrado!!"); cleanAddress() } // Not Found
+                , 400: function (msg) { $("#msg-cep").html("* Não foi possivel realizar a pesquisa."); cleanAddress() } // Bad Request
+                , 404: function (msg) { $("#msg-cep").html("* CEP não encontrado."); cleanAddress() } // Not Found
             },
             error: function () {
-                $("#msg-cep").html("CEP não encontrado!!");
+                $("#msg-cep").html("* Não foi possivel realizar a pesquisa.");
                 cleanAddress()
             }
         });
+        $('#search_cep').removeClass("disabled");
     });
 
     function cleanAddress() {
